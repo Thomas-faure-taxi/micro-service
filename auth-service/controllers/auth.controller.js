2692d6c6b72c91ef9a52ccff02a649dff769e111
Thomas <thomas@faure.taxi>
2025-07-10 12:33:03 +0200

    add message error 404

--- auth-service/controllers/auth.controller.js
+++ auth-service/controllers/auth.controller.js
@@ -19,10 +19,10 @@ exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-    if (!user) return res.status(401).json({ error: error });
+    if (!user) return res.status(404).json({ error: "utilisateur non trouvé" });
 
     const valid = await bcrypt.compare(password, user.password);
-    if (!valid) return res.status(401).json({ error: error });
+    if (!valid) return res.status(401).json({ error: "mauvais mot de passe" });
 
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
     res.json({ token });
